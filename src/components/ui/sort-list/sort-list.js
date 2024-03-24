6220b8180981015c1181df18a1882598d1961287
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2024-03-24 12:27:06 +0300

    enhancement - add destroy method inside SortList Class

--- src/components/ui/sort-list/sort-list.js
+++ src/components/ui/sort-list/sort-list.js
@@ -10,10 +10,10 @@
       this.btn = this.root.querySelector('.sort-list__btn');
       this.list = this.root.querySelector('.sort-list__list-wrapper');
 
-      this.btn?.addEventListener('click', this.toggleList);
+      this.btn?.addEventListener('click', this.toggleListHandler);
     }
 
-    toggleList = () => {
+    toggleListHandler = () => {
       const visible = '_visible';
 
       if (!this.list) return;
@@ -21,6 +21,10 @@
       this.list.classList[
         this.list.classList.contains(visible) ? 'remove' : 'add'
       ](visible);
+    };
+
+    destroy = () => {
+      this.btn?.removeEventListener('click', this.toggleListHandler);
     };
   }
 
