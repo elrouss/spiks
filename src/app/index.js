199608b6e5655d993b8479fc7b2a6c2d0051e1ee
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2024-03-27 01:55:53 +0300

    feature/filters-list create accordion

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 // =require ../core/config/js/index.js
 // =require ../core/lib/js/index.js
 
+// =require ../components/blocks/filters-accordion/filters-accordion.js
+
 // =require ../components/ui/range-slider/range-slider.js
 // =require ../components/ui/sort-list/sort-list.js
 
