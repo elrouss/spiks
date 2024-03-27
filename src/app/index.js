93a124f774daf59545d42dfa0f51923b5a6d329f
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2024-03-28 00:19:14 +0300

    pages/index - add logic with adding/removing badges

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,12 @@
 // =require ../core/config/js/index.js
 // =require ../core/lib/js/index.js
 
+// SECTIONS
+// =require ../components/sections/products/products.js
+
+// BLOCKS
 // =require ../components/blocks/filters-accordion/filters-accordion.js
 
+// UI
 // =require ../components/ui/range-slider/range-slider.js
 // =require ../components/ui/sort-list/sort-list.js
